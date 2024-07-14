f517e8db0a3d399d25d8548e6db25338eb3bf996
Avnish Kumar <69550090+theavnishkumar@users.noreply.github.com>
2024-07-14 13:51:14 +0530

    updated dburl and port

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import express from 'express';
-import {loginRestrict, userAccountRestrict} from './middleware/auth.js';
+import { loginRestrict, userAccountRestrict } from './middleware/auth.js';
 import { router } from './routes/url.js';
 import userRouter from './routes/user.js';
 import mongoDBConnection from './connection.js';
-import {urls} from './models/url.js';
+import { urls } from './models/url.js';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+dotenv.config().parsed;
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 
-mongoDBConnection('mongodb://localhost:27017/shortUrl')
+mongoDBConnection(process.env.MONGO_URL)
     .then(() => { console.log('Connected to MongoDB'); })
     .catch((err) => { console.log('Error: ', err); });
 
@@ -22,10 +24,10 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/',userAccountRestrict ,async(req, res) => {
-    if(!req.user) return res.redirect('/login');
-    const showUrl = await urls.find({createdBy:req.user._id});
-    return res.render('index', { url: showUrl, id: req.user.name})
+app.get('/', userAccountRestrict, async (req, res) => {
+    if (!req.user) return res.redirect('/login');
+    const showUrl = await urls.find({ createdBy: req.user._id });
+    return res.render('index', { url: showUrl, id: req.user.name })
 });
 
 app.get('/login', (req, res) => {
@@ -38,15 +40,15 @@ app.get('/register', (req, res) => {
 
 app.get('/:shortUrl', async (req, res) => {
     const shortUrl = req.params.shortUrl;
-    const entry=  await urls.findOneAndUpdate(
-        {shortUrl},
-        {$push:{clicks:{timestamp:Date.now()}}}
+    const entry = await urls.findOneAndUpdate(
+        { shortUrl },
+        { $push: { clicks: { timestamp: Date.now() } } }
     );
-    if(!entry) return res.render('error');
+    if (!entry) return res.render('error');
     res.redirect(entry.url);
 })
 
-app.use('/url',  loginRestrict ,router);
+app.use('/url', loginRestrict, router);
 app.use('/user', userRouter);
 
 app.listen(port, () => {
