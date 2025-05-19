9f2eee08d14e27a33d1ea95fbad8801482979f52
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-05-19 09:27:50 +0530

    update origin env

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
