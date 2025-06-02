dfd0442c894de9215bdc18b1e66819c87c628f95
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-02 22:30:49 +0530

    added change password function

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { handleGetUser, handleDeleteUser } from "../controllers/user.controller.js";
+import { handleGetUser, handleDeleteUser, handleChangePassword } from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
 userRouter.get('/', handleGetUser);
-userRouter.post('/:id',handleDeleteUser);
+userRouter.post('/:id', handleDeleteUser);
+userRouter.patch("/", handleChangePassword);
 
 export default userRouter;
