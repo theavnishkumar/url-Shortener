74df5624c932bac62d27c7bd466e57acd30f8d41
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-02 22:31:00 +0530

    added change password api

---
+++ client/src/api/user.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const VITE_USER_API = import.meta.env.VITE_USER_API;
+
+export const changePassword = async (formData) => {
+    try {
+        const res = await axios.patch(`${VITE_USER_API}`,
+            formData,
+            { withCredentials: true }
+        );
+        return res.data;
+    } catch (error) {
+        console.log(error?.response?.data?.error || "Can't update password")
+        throw error;
+    }
+}
