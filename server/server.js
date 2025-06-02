dd6e95394a480698e4d3bfbba203ecefdf8dcdbf
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-02 22:27:05 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
 }));
 
