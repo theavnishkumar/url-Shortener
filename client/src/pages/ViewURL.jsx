3b9b92f997d18085c81fbab87453795524a3c01d
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-06-30 23:33:49 +0530

    spinner update

--- client/src/pages/ViewURL.jsx
+++ client/src/pages/ViewURL.jsx
@@ -94,9 +94,9 @@ export default function ViewURL() {
 
 const Spinner = () => {
   return (
-    <div className="flex-col gap-4 w-full flex items-center justify-center">
-      <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
-        <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"></div>
+    <div className="flex items-center justify-center">
+      <div className="flex justify-center text-center">
+        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-indigo-600"></div>
       </div>
     </div>
   );
