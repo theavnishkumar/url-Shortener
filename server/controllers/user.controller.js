8c6197f63c95ff24ff5a9a48ae39b731e8edc3e9
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-07-03 20:42:02 +0530

    limit login history

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -75,6 +75,9 @@ export const getLoginHistory = async (req, res) => {
             },
             {
                 $sort: { loginAt: -1 }
+            },
+            {
+                $limit: 10
             }
         ]);
 
