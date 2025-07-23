bc23f71f2732de5861844e0726ca2adb6da75971
Avnish Kumar <theavnishkumar@users.noreply.github.com>
2025-07-23 22:57:01 +0530

    Implement admin access control in AdminLayout with loading state handling

--- client/src/layout/AdminLayout.jsx
+++ client/src/layout/AdminLayout.jsx
@@ -1,10 +1,26 @@
-import {Outlet} from "react-router"
+import {Outlet, useNavigate} from "react-router"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import ScrollToTop from "../utils/ScrollToTop";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 
 export const AdminLayout = () => {
+  const navigate = useNavigate();
+  const { user, loading } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login");
+    }
+  }, [loading, user, navigate]);
+
+  if (loading || !user) return <LoadingSpinner />;
+  if(user.role!=="admin") return <>Only admin allowed</>
   return (
     <>
+    <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer/>
